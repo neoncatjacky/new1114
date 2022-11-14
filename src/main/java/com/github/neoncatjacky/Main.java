9fbe3eddcaafc4b25737ccb3e7c699cee79cceb5
neoncat <jackylanpoison97141@gmail.com>
2022-11-14 14:52:34 +0800

    ver 1.1 add

--- src/main/java/com/github/neoncatjacky/Main.java
+++ src/main/java/com/github/neoncatjacky/Main.java
@@ -3,5 +3,6 @@ package com.github.neoncatjacky;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello hahahaworld!");
+        System.out.println("lalala!");
     }
 }
